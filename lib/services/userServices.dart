671fc24954a818c84df1ce2c36bff1db83eb80ea
Marie P. Lopez Cama <yaminelopezcama@gmail.com>
2021-04-30 02:30:02 +0200

    pequeño arreglo url userservices

--- lib/services/userServices.dart
+++ lib/services/userServices.dart
@@ -3,11 +3,11 @@ import 'package:fluttertoast/fluttertoast.dart';
 
 class UserServices {
   Dio dio = new Dio();
+  var url = "http://localhost:4000/api/users/";
 
   login(email, password) async {
-    print(email);
     try {
-      return await dio.post('http://localhost:4000/api/users/logIn',
+      return await dio.post(url+'logIn',
           data: {"email": email, "password": password},
           options: Options(contentType: Headers.formUrlEncodedContentType));
     } on DioError catch (e) {
@@ -18,11 +18,16 @@ class UserServices {
           timeInSecForIosWeb: 1);
     }
   }
-  
-  register(name, email, password)async{
+
+  register(name, email, password) async {
     try {
-      return await dio.post('http://localhost:4000/api/users/signUp',
-          data: {"username":name, "email": email, "password": password, "role":1},
+      return await dio.post(url+'signUp',
+          data: {
+            "username": name,
+            "email": email,
+            "password": password,
+            "role": 1
+          },
           options: Options(contentType: Headers.formUrlEncodedContentType));
     } on DioError catch (e) {
       Fluttertoast.showToast(
