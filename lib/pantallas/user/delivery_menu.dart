e1dd0b8486f983c69d9a9492540d40b7b9b4ea74
Marie P. Lopez Cama <yaminelopezcama@gmail.com>
2021-05-12 11:02:39 +0200

    menu delivery

--- lib/pantallas/user/delivery_menu.dart
+++ lib/pantallas/user/delivery_menu.dart
@@ -1,10 +1,109 @@
 import 'package:flutter/cupertino.dart';
+import 'package:rlbasic/models/_aux.dart';
+import 'package:flutter/material.dart';
+import 'package:rlbasic/my_navigator.dart';
 
 class DeliveryMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      home: DeliveryMenuScreen(),
     );
+  }
+}
+
+class DeliveryMenuScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(            
+            automaticallyImplyLeading: false,
+            backgroundColor: Color(0xff5808e5),
+            bottom: TabBar(
+              indicatorColor: Colors.white,
+              tabs: [
+                Tab(text: 'GET READY', icon: Icon(Icons.alarm_add)),
+                Tab(text: 'PRODUCT TO PICK UP', icon: Icon(Icons.alarm_on)),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Center(child: Text('')),
+              Center(child: Text('')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Menu extends StatelessWidget {
+  final Aux aux = new Aux("id", "token", "userName");
+  //final Aux aux;
+  // DeliveryMenu({required this.aux});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(aux.userName, semanticsLabel: "Bienvenido"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Perfil',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+                leading: Icon(Icons.add_business_rounded),
+                title: Text('Buscar productos'),
+                onTap: () {
+                  MyNavigator.goToSearchProducts(context);
+                }),
+            ListTile(
+                leading: Icon(Icons.apartment),
+                title: Text('Mis productos almacenados'),
+                onTap: () {
+                  Navigator.of(context).pushNamed("");
+                }),
+            ListTile(
+                leading: Icon(Icons.motorcycle),
+                title: Text('Productos para entregar'),
+                onTap: () {
+                  MyNavigator.goToUserDeliveryMenu(context);
+                }),
+            ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('Configuración'),
+                onTap: () {
+                  MyNavigator.goToConfigUser(context);
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DeliveryList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
