6ce6225f820fbe5f472e097344a1336b1a0557f5
Carlos Méndez <c.mendez@tobeit.es>
2021-05-29 18:23:43 +0200

    Login with google.

--- lib/services/userServices.dart
+++ lib/services/userServices.dart
@@ -1,5 +1,14 @@
 import 'package:dio/dio.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:rlbasic/my_navigator.dart';
+
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  scopes: [
+    'email',
+    'profile',
+  ],
+);
 
 class UserServices {
   Dio dio = new Dio();
@@ -21,6 +30,29 @@ class UserServices {
     }
   }
 
+  Future<void> loginGoogle() async {
+    try {
+      await _googleSignIn.signIn();
+      print(_googleSignIn.currentUser!.email);
+      print(_googleSignIn.currentUser!.displayName);
+      print(_googleSignIn.currentUser!.photoUrl);
+      try {
+        await dio.post(url + 'logInGoogle',
+            data: {"email": _googleSignIn.currentUser!.email, "userName": _googleSignIn.currentUser!.displayName, "avatar": _googleSignIn.currentUser!.photoUrl},
+            options: Options(contentType: Headers.formUrlEncodedContentType));
+        MyNavigator.goTo
+      } on DioError catch (e) {
+        Fluttertoast.showToast(
+            msg: e.response?.data['msg'],
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1);
+      }
+    } catch (error) {
+      print(error);
+    }
+  }
+
   register(name, email, password) async {
     try {
       return await dio.post(url + 'signUp',
@@ -30,7 +62,7 @@ class UserServices {
             "password": password,
           },
           options: Options(contentType: Headers.formUrlEncodedContentType));
-          
+
     } on DioError catch (e) {
       Fluttertoast.showToast(
           msg: e.response?.data['msg'],
